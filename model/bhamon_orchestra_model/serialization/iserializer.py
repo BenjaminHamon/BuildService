a3cc9f3e3a30013b727c08c8d324cf9e62c9c620
Benjamin Hamon <hamon.benjamin@gmail.com>
2020-11-11 15:57:55 +0100

    Add json serializer

---
+++ model/bhamon_orchestra_model/serialization/iserializer.py
@@ -0,0 +1,25 @@
+import abc
+from typing import Any, Optional
+
+
+class ISerializer(abc.ABC):
+
+
+	@abc.abstractmethod
+	def serialize_to_file(self, path: str, value: Optional[Any]) -> None:
+		pass
+
+
+	@abc.abstractmethod
+	def deserialize_from_file(self, path: str) -> Optional[Any]:
+		pass
+
+
+	@abc.abstractmethod
+	def serialize_to_string(self, value: Optional[Any]) -> str:
+		pass
+
+
+	@abc.abstractmethod
+	def deserialize_from_string(self, serialized_value: str) -> Optional[Any]:
+		pass
